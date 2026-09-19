0a983e44318ee5d8f65f899266ec21dda14dbd81
vrvivek1510-oss <vr.vivek.1510@gmail.com>
2026-09-19 11:54:42 +0530

    New-Changes

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+  protected readonly dots: { left: number; speed: number; delay: number }[] = Array.from(
+    { length: 14 },
+    (_, index) => ({
+      left: (index * 7.3 + 3) % 96,
+      speed: +(8 + ((index * 5.3) % 8)).toFixed(2),
+      delay: +((index * 0.9) % 8).toFixed(2),
+    }),
+  );
+}
